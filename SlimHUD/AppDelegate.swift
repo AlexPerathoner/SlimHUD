b7b2914481ed375ff0e7636b2fa92855f51eacdb
Alex Perathoner <alex.perathoner@gmail.com>
2020-02-16 21:51:52 +0100

    Initial Commit

---
+++ SlimHUD/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlimHUD
+//
+//  Created by Alex Perathoner on 16/02/2020.
+//  Copyright © 2020 Alex Perathoner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SlimHUD/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SlimHUD
+//
+//  Created by Alex Perathoner on 16/02/2020.
+//  Copyright © 2020 Alex Perathoner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
