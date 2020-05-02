610df781cf70f49123787f4ef96a1f0b756fa38a
Alex Perathoner <alex.perathoner@gmail.com>
2020-05-02 17:30:12 +0200

    Adding check for single settings window Now only one window at the time can be displayed - Still working on the same feature for the About window Also simplifying SettingsWindowController

--- SlimHUD/Settings/SettingsWindowController.swift
+++ SlimHUD/Settings/SettingsWindowController.swift
@@ -9,11 +9,7 @@
 import Cocoa
 
 class SettingsWindowController: NSWindowController, NSWindowDelegate {
-	// MARK: - Preview
-	
-	@objc func windowWillClose() {
-		showingPreviewHUD(false)
-	}
+	static var hasIstance = false
 	
 	
 	var previewTimer: Timer?
@@ -25,32 +21,24 @@ class SettingsWindowController: NSWindowController, NSWindowDelegate {
 			}
 			RunLoop.current.add(previewTimer!, forMode: .eventTracking)
 		} else {
-			NotificationCenter.default.removeObserver(self, name: .init("NSWindowWillCloseNotification"), object: window)
 			previewTimer?.invalidate()
 			previewTimer = nil
 		}
 	}
 	
 	override func windowDidLoad() {
-		NotificationCenter.default.addObserver(self, selector: #selector(windowWillClose), name: .init("NSWindowWillCloseNotification"), object: window)
-		showingPreviewHUD(true)
+		SettingsWindowController.hasIstance = true
 		super.windowDidLoad()
-		self.window?.delegate = self
+		showingPreviewHUD(true)
+		window?.delegate = self
 	}
 	
 	
-	func windowShouldClose(_ sender: NSWindow) -> Bool {
-
-		#if DEBUG
-		let closingCtl = sender.contentViewController!
-		let closingCtlClass = closingCtl.className
-		print("\(closingCtlClass) is closing")
-		#endif
-
-
-		sender.contentViewController = nil // will force deinit.
-
-		return true // allow to close.
+	func windowWillClose(_ notification: Notification) {
+		
+		SettingsWindowController.hasIstance = false
+		showingPreviewHUD(false)
+		
 	}
 	
 }
