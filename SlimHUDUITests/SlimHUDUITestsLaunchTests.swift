bcef582215b659035ed6d78c56a6cbdafcc89e91
Alex Perathoner <alex.perathoner@gmail.com>
2022-12-22 20:30:47 +0100

    add ui tests

---
+++ SlimHUDUITests/SlimHUDUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SlimHUDUITestsLaunchTests.swift
+//  SlimHUDUITests
+//
+//  Created by Alex Perathoner on 22/12/22.
+//  Copyright © 2022 Alex Perathoner. All rights reserved.
+//
+
+import XCTest
+
+final class SlimHUDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
