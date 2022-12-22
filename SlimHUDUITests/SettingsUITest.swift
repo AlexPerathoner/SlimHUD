54bb59429f704f13ffbe374aa9f2abbb5b530f72
Alex Perathoner <alex.perathoner@gmail.com>
2022-12-22 21:22:15 +0100

    fixes

--- SlimHUDUITests/SettingsUITest.swift
+++ SlimHUDUITests/SettingsUITest.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 final class SettingsUITest: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
