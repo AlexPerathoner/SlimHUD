0187018e01836ff0fc431374c80ec6f9af97d058
Alex Perathoner <alex.perathoner@gmail.com>
2022-12-23 20:25:42 +0100

    all development sign

--- SlimHUDTests/Constants.swift
+++ SlimHUDTests/Constants.swift
@@ -1,23 +1,23 @@
+////
+////  Constants.swift
+////  SlimHUDTests
+////
+////  Created by Alex Perathoner on 21/12/22.
+////  Copyright © 2022 Alex Perathoner. All rights reserved.
+////
 //
-//  Constants.swift
-//  SlimHUDTests
+//import Foundation
 //
-//  Created by Alex Perathoner on 21/12/22.
-//  Copyright © 2022 Alex Perathoner. All rights reserved.
-//
-
-import Foundation
-
-final class Constants {
-    public struct Hud {
-        static let ShortEdge: CGFloat = 47
-        static let LongEdge: CGFloat = 297
-    }
-    public struct Screen {
-        static let Width: CGFloat = 1440
-        static let Height: CGFloat = 900
-        public static let MenuBarSize: CGFloat = 25
-        public static let DockSize: CGFloat = 62
-        public static let Frame = NSRect(x: 0, y: 0, width: Screen.Width, height: Screen.Height)
-    }
-}
+//final class Constants {
+//    public struct Hud {
+//        static let ShortEdge: CGFloat = 47
+//        static let LongEdge: CGFloat = 297
+//    }
+//    public struct Screen {
+//        static let Width: CGFloat = 1440
+//        static let Height: CGFloat = 900
+//        public static let MenuBarSize: CGFloat = 25
+//        public static let DockSize: CGFloat = 62
+//        public static let Frame = NSRect(x: 0, y: 0, width: Screen.Width, height: Screen.Height)
+//    }
+//}
