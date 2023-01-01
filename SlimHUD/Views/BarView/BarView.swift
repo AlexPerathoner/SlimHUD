becbbbec34d2c6624f2fcf114c064c011d4dbf2d
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-01 22:11:52 +0100

    refactor barview set Icons rotation

--- SlimHUD/Views/BarView/BarView.swift
+++ SlimHUD/Views/BarView/BarView.swift
@@ -13,5 +13,17 @@ class BarView: NSView {
 
     @IBOutlet weak var bar: ProgressBar!
     @IBOutlet var image: NSImageView!
+    
+    public func setIconRotation(isHorizontal: Bool) {
+        if isHorizontal {
+            while image.boundsRotation.truncatingRemainder(dividingBy: 360) != 90 {
+                image.rotate(byDegrees: 90)
+            }
+        } else {
+            while image.boundsRotation.truncatingRemainder(dividingBy: 360) != 0 {
+                image.rotate(byDegrees: 90)
+            }
+        }
+    }
 
 }
