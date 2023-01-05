5d254103cfea7067ae37526d6f1f4562d01bafcc
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-05 20:56:08 +0100

    a

--- SlimHUDUITests/HudsUITests.swift
+++ SlimHUDUITests/HudsUITests.swift
@@ -14,18 +14,37 @@ final class HudsUITest: SparkleUITests {
         continueAfterFailure = false
         app.launch()
     }
-
+    
     func testTriggerVolumeHud() throws {
         XCTAssert(app.windows.count == 0)
-        changeVolume()
+        do {
+            try changeVolume()
+        } catch {
+            XCTAssertEqual(error.localizedDescription, "")
+        }
         usleep(500000)
         XCTAssert(app.windows.count >= 1)
     }
-
-    private func changeVolume() {
+    
+    func testTriggerVolumeHud2() throws {
+        XCTAssert(app.windows.count == 0)
         let task = Process()
         task.launchPath = "/usr/bin/osascript"
-        task.arguments = ["-e set volume output volume 30", "-e set volume output volume 10"]
-        task.launch()
+        task.arguments = ["-e display alert \"hey\""]
+        try task.run()
+        do {
+            try changeVolume()
+        } catch {
+            XCTAssertEqual(error.localizedDescription, "")
+        }
+        usleep(1500000)
+        XCTAssert(app.windows.count >= 1)
+    }
+
+    private func changeVolume() throws {
+        let task = Process()
+        task.launchPath = "/usr/bin/osascript"
+        task.arguments = ["-e set volume output volume 50", "-e set volume output volume 10"]
+        try task.run()
     }
 }
