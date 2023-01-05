dc5eee59586f2b21aeff0e9fa189a0c1283b9cb6
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-05 05:45:50 +0100

    a

--- SlimHUDUITests/HudsUITests.swift
+++ SlimHUDUITests/HudsUITests.swift
@@ -14,12 +14,31 @@ final class HudsUITest: SparkleUITests {
 
         app.launch()
     }
-
+    
     func testTriggerVolumeHud() throws {
         XCTAssert(app.windows.count == 0)
         do {
-            let a = try AppleScriptRunner.run(script: "return output volume of (get volume settings)") // XCTAssertEqual failed: ("The operation couldn’t be completed. (SlimHUDUITests.AppleScriptError error 2.)") is not equal to ("")
+            try AppleScriptRunner.run(script: "set volume with output muted") // XCTAssertEqual failed: ("The operation couldn’t be completed. (SlimHUDUITests.AppleScriptError error 2.)") is not equal to ("")
+        } catch {
+            XCTAssertEqual(error.localizedDescription, "")
+            NSLog("Applescript returned empty output")
+        }
+        usleep(300000)
+        do {
+            let a = try AppleScriptRunner.run(script: "set volume output volume 10")
             XCTAssertEqual(a, "")
+        } catch AppleScriptError.emptyOutput {
+            NSLog("Applescript returned empty output")
+        }
+
+        usleep(1000000)
+        XCTAssert(app.windows.count >= 1)
+    }
+    
+    func testTriggerVolumeHud1() throws {
+        XCTAssert(app.windows.count == 0)
+        do {
+            try AppleScriptRunner.run(script: "set volume with output muted") // XCTAssertEqual failed: ("The operation couldn’t be completed. (SlimHUDUITests.AppleScriptError error 2.)") is not equal to ("")
         } catch {
             XCTAssertEqual(error.localizedDescription, "")
             NSLog("Applescript returned empty output")
