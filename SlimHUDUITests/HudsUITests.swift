0f30aac6b6f48412389333a576d525ba18bf241d
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-05 03:38:36 +0100

    add volume hud test

--- SlimHUDUITests/HudsUITests.swift
+++ SlimHUDUITests/HudsUITests.swift
@@ -20,6 +20,19 @@ final class HudsUITest: SparkleUITests {
     }
     
     func testTriggerVolumeHud() throws {
+        XCTAssert(app.windows.count == 0)
+        do {
+            try AppleScriptRunner.run(script: "set volume output volume 40")
+        } catch AppleScriptError.emptyOutput {
+            NSLog("Applescript returned empty output")
+        }
+        do {
+            try AppleScriptRunner.run(script: "set volume output volume 15")
+        } catch AppleScriptError.emptyOutput {
+            NSLog("Applescript returned empty output")
+        }
         
+        usleep(500000)
+        XCTAssert(app.windows.count == 1)
     }
 }
