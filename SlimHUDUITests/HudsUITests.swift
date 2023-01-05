9164623892d28bb23d1824cb7603074c1b880270
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-05 20:42:10 +0100

    using process()

--- SlimHUDUITests/HudsUITests.swift
+++ SlimHUDUITests/HudsUITests.swift
@@ -5,53 +5,27 @@
 //  Created by Alex Perathoner on 05/01/23.
 //
 
+import Foundation
 import XCTest
 
 final class HudsUITest: SparkleUITests {
     var app = XCUIApplication()
     override func setUpWithError() throws {
         continueAfterFailure = false
-
         app.launch()
     }
-    
+
     func testTriggerVolumeHud() throws {
         XCTAssert(app.windows.count == 0)
-        do {
-            try AppleScriptRunner.run(script: "display alert \"hey\"") // XCTAssertEqual failed: ("The operation couldn’t be completed. (SlimHUDUITests.AppleScriptError error 2.)") is not equal to ("")
-        } catch {
-            XCTAssertEqual(error.localizedDescription, "")
-            NSLog("Applescript returned empty output")
-        }
-        usleep(300000)
-        do {
-            let a = try AppleScriptRunner.run(script: "set volume output volume 10")
-            XCTAssertEqual(a, "")
-        } catch AppleScriptError.emptyOutput {
-            NSLog("Applescript returned empty output")
-        }
-
-        usleep(1000000)
+        changeVolume()
+        usleep(500000)
         XCTAssert(app.windows.count >= 1)
     }
     
-    func testTriggerVolumeHud1() throws {
-        XCTAssert(app.windows.count == 0)
-        do {
-            try AppleScriptRunner.run(script: "set volume with output muted") // XCTAssertEqual failed: ("The operation couldn’t be completed. (SlimHUDUITests.AppleScriptError error 2.)") is not equal to ("")
-        } catch {
-            XCTAssertEqual(error.localizedDescription, "")
-            NSLog("Applescript returned empty output")
-        }
-        usleep(300000)
-        do {
-            let a = try AppleScriptRunner.run(script: "set volume output volume 10")
-            XCTAssertEqual(a, "")
-        } catch AppleScriptError.emptyOutput {
-            NSLog("Applescript returned empty output")
-        }
-
-        usleep(1000000)
-        XCTAssert(app.windows.count >= 1)
+    private func changeVolume() {
+        let task = Process()
+        task.launchPath = "/usr/bin/osascript"
+        task.arguments = ["-e set volume output volume 30", "-e set volume output volume 10"]
+        task.launch()
     }
 }
