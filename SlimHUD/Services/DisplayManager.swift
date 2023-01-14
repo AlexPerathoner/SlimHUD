e1d70f3f8485f6d4ec9cac7b6c56018679fd699f
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-14 19:32:10 +0100

    fix

--- SlimHUD/Services/DisplayManager.swift
+++ SlimHUD/Services/DisplayManager.swift
@@ -135,7 +135,9 @@ class DisplayManager {
         for window in windows {
             if let windowName = window[kCGWindowOwnerName] as? String {
                 // if Window Server or Dock are visible the user is certainly not using fullscreen
-                if windowName == "Window Server" || windowName == "Dock" {return false}
+                if windowName == "Window Server" || windowName == "Dock" {
+                    return false
+                }
                 if window[kCGWindowBounds]?["Height"] as? CGFloat ?? 0 == screenSize.height &&
                     window[kCGWindowBounds]?["Width"] as? CGFloat ?? 0 == screenSize.width &&
                     windowName != "SlimHUD" {
