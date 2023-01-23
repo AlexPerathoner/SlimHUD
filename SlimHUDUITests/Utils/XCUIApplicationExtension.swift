78a53052567135758d0d7e5ed1baf0fc0522ad32
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-23 21:01:06 +0100

    fix tests

--- SlimHUDUITests/Utils/XCUIApplicationExtension.swift
+++ SlimHUDUITests/Utils/XCUIApplicationExtension.swift
@@ -14,7 +14,4 @@ extension XCUIApplication {
     func shouldShowSettingsAtLaunch() {
         launchArguments += ["showSettingsAtLaunch"]
     }
-    func shouldShowSettingsAtLaunch() {
-        launchArguments += ["showSettingsAtLaunch"]
-    }
 }
