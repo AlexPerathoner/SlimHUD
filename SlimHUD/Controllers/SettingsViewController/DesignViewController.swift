c743044499c26564a69268da53a9fa2d4d2ba1ad
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-27 15:59:15 +0100

    WIP design

--- SlimHUD/Controllers/SettingsViewController/DesignViewController.swift
+++ SlimHUD/Controllers/SettingsViewController/DesignViewController.swift
@@ -8,5 +8,38 @@
 import Cocoa
 
 class DesignViewController: NSViewController {
+    weak var delegate: HudsControllerInterface?
+    var settingsManager = SettingsManager.getInstance()
 
+    @IBOutlet weak var thicknessValue: NSTextField!
+    @IBOutlet weak var sizeValue: NSTextField!
+    
+    @IBOutlet weak var thicknessSlider: NSSlider!
+    @IBOutlet weak var sizeSlider: NSSlider!
+    
+    @IBAction func sizeSlider(_ sender: NSSlider) {
+        sizeValue.stringValue = String(sender.integerValue)
+        settingsManager.barHeight = sender.integerValue
+        delegate?.setHeight(height: CGFloat(sender.integerValue))
+    }
+
+    @IBAction func thicknessSlider(_ sender: NSSlider) {
+        thicknessValue.stringValue = String(sender.integerValue)
+        settingsManager.barThickness = sender.integerValue
+        delegate?.setThickness(thickness: CGFloat(sender.integerValue))
+    }
+    @IBAction func sizeText(_ sender: NSTextField) {
+        if let newValue = Int(sender.stringValue) {
+            sizeSlider.integerValue = newValue
+            settingsManager.barHeight = sender.integerValue
+            delegate?.setHeight(height: CGFloat(newValue))
+        }
+    }
+    @IBAction func thicknessText(_ sender: NSTextField) {
+        if let newValue = Int(sender.stringValue) {
+            thicknessSlider.integerValue = newValue
+            settingsManager.barThickness = sender.integerValue
+            delegate?.setThickness(thickness: CGFloat(newValue))
+        }
+    }
 }
