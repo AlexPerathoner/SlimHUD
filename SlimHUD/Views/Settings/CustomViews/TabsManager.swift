cf22cf50cb628842f2ea06328921eb07706e9e04
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-27 15:48:34 +0100

    fix viewcontroller mechanism

--- SlimHUD/Views/Settings/CustomViews/TabsManager.swift
+++ SlimHUD/Views/Settings/CustomViews/TabsManager.swift
@@ -11,24 +11,35 @@ class TabsManager: NSView {
     var tabs: TabsView = TabsView(frame: .init(x: 0, y: 414, width: 400, height: 86))
     var contentViews: [NSView] = []
     var view: NSView?
+    
+    var configVC: ConfigViewController?
+    var designVC: DesignViewController?
+    var styleVC: StyleViewController?
+    var aboutVC: AboutViewController?
 
     override func awakeFromNib() {
-        addSubview(tabs)
-        tabs.tabsContentView = self
-        tabs.awakeFromNib()
+        if configVC == nil {
+            addSubview(tabs)
+            tabs.tabsContentView = self
+            tabs.awakeFromNib()
 
-        let storyboard = NSStoryboard(name: "Settings", bundle: nil)
-        if let config = storyboard.instantiateController(withIdentifier: "config") as? ConfigViewController,
-           let design = storyboard.instantiateController(withIdentifier: "design") as? DesignViewController,
-           let style = storyboard.instantiateController(withIdentifier: "style") as? StyleViewController,
-           let about = storyboard.instantiateController(withIdentifier: "about") as? AboutViewController {
-            for view in [config.view, design.view, style.view, about.view] {
-                contentViews.append(view)
-                view.setFrameOrigin(.zero)
-                addSubview(view)
+            let storyboard = NSStoryboard(name: "Settings", bundle: nil)
+            if let config = storyboard.instantiateController(withIdentifier: "config") as? ConfigViewController,
+               let design = storyboard.instantiateController(withIdentifier: "design") as? DesignViewController,
+               let style = storyboard.instantiateController(withIdentifier: "style") as? StyleViewController,
+               let about = storyboard.instantiateController(withIdentifier: "about") as? AboutViewController {
+                for view in [config.view, design.view, style.view, about.view] {
+                    contentViews.append(view)
+                    view.setFrameOrigin(.zero)
+                    addSubview(view)
+                }
+                configVC = config
+                designVC = design
+                styleVC = style
+                aboutVC = about
+                
+                selectItem(index: 0)
             }
-
-            selectItem(index: 0)
         }
         super.awakeFromNib()
     }
