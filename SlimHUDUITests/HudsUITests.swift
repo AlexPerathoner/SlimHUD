9eb2d0df4f537aac58d58bb6c11a012db2a3ba88
Alex Perathoner <alex.perathoner@gmail.com>
2023-03-04 18:50:21 +0100

    fix testTriggerVolumeHud

--- SlimHUDUITests/HudsUITests.swift
+++ SlimHUDUITests/HudsUITests.swift
@@ -18,19 +18,27 @@
 
     func testTriggerVolumeHud() throws {
         XCTAssert(app.windows.count == 0)
-        do {
-            try changeVolume()
-        } catch {
-            XCTAssertEqual(error.localizedDescription, "")
+        DispatchQueue.global().async {
+            do {
+                try self.changeVolume()
+            } catch {
+                XCTAssertEqual(error.localizedDescription, "")
+            }
         }
-        usleep(500000)
+        sleep(1)
         XCTAssertTrue(isVolumeHudVisible(app: app))
     }
 
     private func changeVolume() throws {
         let task = Process()
         task.launchPath = "/usr/bin/osascript"
-        task.arguments = ["-e set volume output volume 50", "-e delay 1", "-e set volume output volume 20"]
+        task.arguments = ["-e set volume output volume 50",
+                          "-e delay 1",
+                          "-e set volume output volume 20",
+                          "-e delay 1",
+                          "-e set volume output volume 50",
+                          "-e delay 1",
+                          "-e set volume output volume 20"]
         try task.run()
     }
  }
